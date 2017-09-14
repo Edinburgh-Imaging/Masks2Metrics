4546b6fbea01401cd422c0d10696de8ba9498e2f
smikhael <smikhael@users.noreply.github.com>
2017-09-14 16:09:28 +0100

    Cleaned up comment formatting at beginning of code

--- find_closestY_at_X1.m
+++ find_closestY_at_X1.m
@@ -1,24 +1,23 @@
-%% This function identifies the closest Y coordinate on either:
-%% 1. the GM curve array corresponding to an X coordinate on the WM curve (Xwm, Ywm). i.e., finds on GM: Ygm at Xwm (Xwm, Ygm). In this case it returns closest Ygm
-%% 2. the WM curve array corresponding to an X coordinate on the GM curve (Xgm, Ygm). i.e., finds on WM: Ywm at Xgm (Xgm, Ywm). In this case it returns closest Ywm
-%% X1 is Xwm for the first case, and Xgm for the 2nd
+function [X_closest, Y_closest] = find_closestY_at_X1(X1,Y1,curve)
+% This function identifies the closest Y coordinate on either:
+% 1. the GM curve array corresponding to an X coordinate on the WM curve (Xwm, Ywm). i.e., finds on GM: Ygm at Xwm (Xwm, Ygm). In this case it returns closest Ygm
+% 2. the WM curve array corresponding to an X coordinate on the GM curve (Xgm, Ygm). i.e., finds on WM: Ywm at Xgm (Xgm, Ywm). In this case it returns closest Ywm
+% X1 is Xwm for the first case, and Xgm for the 2nd
 % 
-
-%Inputs: 
+% Inputs: 
 %   X1,Y1: the x- and y-coordinates of the voxel under investigation on
 %   either the WM or GM mask in the
 %   current slice
 %   curve: the array of (x,y) coordinates that make up the corresponding GM
 %   or WM mask
 %
-%Outputs:
+% Outputs:
 %   Y_closest: the y-coordinate of the voxel on 'curve' (i.e., the GM/WM
 %   mask) that is closest to X1
 %   X_closest: the corresponding x-coordinate of the voxel on 'curve' (i.e., the GM/WM
 %   mask) that is closest to X1
-
-
-function [X_closest, Y_closest] = find_closestY_at_X1(X1,Y1,curve)
+%
+% Author: S Mikhael - 26 June 2017
 
 Xcurve_array = curve(:,1); %identify all x coordinates of the curve array
 Xarray = abs(Xcurve_array-X1); %taking the difference to identify the closest coords to X1, i.e., with min distance to X1
