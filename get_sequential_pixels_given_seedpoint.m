af9ed5e039413b86bc9458131835efa042f9e86c
smikhael <smikhael@users.noreply.github.com>
2017-10-12 13:52:09 +0100

    Added copyright

--- get_sequential_pixels_given_seedpoint.m
+++ get_sequential_pixels_given_seedpoint.m
@@ -19,7 +19,7 @@ function [array,new_ends] = get_sequential_pixels_given_seedpoint(ROI_slice,ends
 %   slice
 %   new_ends: new/final endpoints of the mask in the current slice
 %
-% Author: S Mikhael - 26 June 2017
+% Masks2Metrics Copyright (C) 2017 S. Mikhael
 
 disp('Identifying sequence of points ..');
 hold on
