bdfde0cbff7f177de6578b3af12aaab5c8e56b45
smikhael <smikhael@users.noreply.github.com>
2017-10-19 23:39:06 +0100

    Updated copyright line

--- find_closestX_at_Y1.m
+++ find_closestX_at_Y1.m
@@ -15,7 +15,7 @@
 %   Y_closest: the y-coordinate of the voxel on 'curve' (i.e., the GM/WM
 %   mask) that is closest to Y1
 %
-% Masks2Metrics Copyright (C) 2017 S. Mikhael
+% Masks2Metrics Copyright (C) 2017 S. Mikhael and C. Gray
 
 function [X_closest, Y_closest] = find_closestX_at_Y1(X1,Y1,curve)
 
